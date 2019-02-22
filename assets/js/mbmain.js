88cc8ff46e73f05d2ac71b0381c50f9fa485ff69
joaocreste <joaocreste@gmail.com>
2019-02-21 22:31:57 -0300

    Add files via upload

--- assets/js/mbmain.js
+++ assets/js/mbmain.js
@@ -12,14 +12,124 @@ var map = new mapboxgl.Map({
     container: 'var_map',
     style: 'mapbox://styles/joaocreste/cjs513dce0z2f1fo4qyx8oob9',
     center: [-49.317,-17.154],
-    zoom: 4
+    zoom: 3.5
 });
 
 var MapView = document.getElementById('MapView')
 var SatView = document.getElementById('SatView')
+var SolarView = document.getElementById('SolarView')
+var WindSpeed = document.getElementById('WindSpeed')
 
+
+MapView.onclick = function (e) {
+    //map.setStyle('mapbox://styles/joaocreste/cjs513dce0z2f1fo4qyx8oob9', diff = false)
+    map.setLayoutProperty('satellite', 'visibility', 'none')
+    map.setLayoutProperty('Solar Irradiance', 'visibility', 'none')
+    map.setLayoutProperty('Wind Speed', 'visibility', 'none') 
+}
+
+SatView.onclick = function (e) {
+    //map.setStyle('mapbox://styles/joaocreste/cjs513dce0z2f1fo4qyx8oob9', diff = false)
+    map.setLayoutProperty('satellite', 'visibility', 'visible')
+    map.setLayoutProperty('Solar Irradiance', 'visibility', 'none') 
+    map.setLayoutProperty('Wind Speed', 'visibility', 'none')
+
+}
+
+SolarView.onclick = function (e) {
+    //map.setStyle('mapbox://styles/joaocreste/cjs513dce0z2f1fo4qyx8oob9', diff = false)
+    map.setLayoutProperty('satellite', 'visibility', 'none')
+    map.setLayoutProperty('Solar Irradiance', 'visibility', 'visible')
+    map.setLayoutProperty('Wind Speed', 'visibility', 'none')
+}
+
+WindSpeed.onclick = function (e) {
+    //map.setStyle('mapbox://styles/joaocreste/cjs513dce0z2f1fo4qyx8oob9', diff = false)
+    map.setLayoutProperty('satellite', 'visibility', 'none')
+    map.setLayoutProperty('Solar Irradiance', 'visibility', 'none')
+    map.setLayoutProperty('Wind Speed', 'visibility', 'visible')
+}
 
 map.on('load', function () {
+
+// - 0. LAYERS TO WORK AS STYLES------------------------------------ //
+
+// --------------------------------------------------------------- //
+
+    map.addLayer({
+      id: 'satellite',
+      source: {"type": "raster",  "url": "mapbox://mapbox.satellite", "tileSize": 256},
+      type: "raster",
+      'layout': {
+            'visibility': 'none',
+        }
+    });
+
+// --------------------------------------------------------------- //
+
+    map.addSource('Solar Irradiance', {
+        type: 'vector',
+        url: 'mapbox://joaocreste.cjip1aul600sfdunvmcotamw7-5i9d4'
+    });
+    map.addLayer({
+        'id': 'Solar Irradiance',
+        'type': 'circle',
+        'source': 'Solar Irradiance',
+        'layout': {
+            'visibility': 'none'
+        },
+        'paint': {
+            'circle-radius': [
+                              "interpolate",
+                              ["exponential", 1.86],
+                              ["zoom"],
+                              0,
+                              5,
+                              7,
+                              20,
+                              10,
+                              150
+                            ],
+            'circle-color': [
+                          "interpolate",
+                          ["linear"],
+                          ["get", "Annual_Mean"],
+                          1778,
+                          "hsl(52, 99%, 72%)",
+                          6465,
+                          "hsl(3, 98%, 64%)"
+                        ]
+        },
+        'source-layer': 'SolarIrradiance'
+    });
+
+// --------------------------------------------------------------- //
+
+    map.addSource('Wind Speed', {
+        type: 'vector',
+        url: 'mapbox://joaocreste.21xtfx1a'
+    });
+    map.addLayer({
+        'id': 'Wind Speed',
+        'type': 'fill',
+        'source': 'Wind Speed',
+        'layout': {
+            'visibility': 'none'
+        },
+        'paint': {
+            'fill-color': [
+                  "interpolate",
+                  ["linear"],
+                  ["get", "windspeed"],
+                  3,
+                  "hsl(186, 83%, 74%)",
+                  9.5,
+                  "hsl(259, 94%, 25%)"
+                ],
+        },
+        'source-layer': 'windspeed'
+    });
+
 
 // - 1. ELECTRICAL DATA ------------------------------------------ //
 
@@ -547,14 +657,6 @@ map.on('load', function () {
 
 // --------------------------------------------------------------- //
 
-MapView.onclick = function (e) {
-    map.setStyle('mapbox://styles/joaocreste/cjs513dce0z2f1fo4qyx8oob9')
-}
-
-SatView.onclick = function (e) {
-    map.setStyle('mapbox://styles/joaocreste/cjiyxqx9s03lc2ro06lktn1kv')
-}
-
 });
 
 
@@ -604,9 +706,6 @@ var description = "<p>Power: <strong>"+power + " MW" +"</strong></p>" +
                     "<p>Hub Height: <strong>"+hub+ " m"+"</strong></p>" +
                     "<p>Diameter: <strong>"+diameter+" m"+"</strong></p>"                
  
-// Ensure that if the map is zoomed out such that multiple
-// copies of the feature are visible, the popup appears
-// over the copy being pointed to.
 while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
     coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
 }
@@ -624,6 +723,40 @@ map.on('mouseenter', 'Wind Turbines', function () {
  
 // Change it back to a pointer when it leaves.
 map.on('mouseleave', 'Wind Turbines', function () {
+    map.getCanvas().style.cursor = '';
+});
+
+
+// --Solar Projects info-- //
+
+map.on('click', 'Solar (Status)', function (e) {
+var coordinates = e.features[0].geometry.coordinates.slice();
+var name = e.features[0].properties.NOME;
+var power = e.features[0].properties.POT_KW/1e3;
+var owner = e.features[0].properties.PROPRIETAR;
+var status = e.features[0].properties.ESTAGIO_1;
+var description = "<p>Name: <strong>"+name +"</strong></p>" +
+                    "<p>Power: <strong>"+power + " MW" +"</strong></p>" +
+                    "<p>Owner: <strong>"+owner+"</strong></p>" +
+                    "<p>Status: <strong>"+status+"</strong></p>"                
+ 
+while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+}
+ 
+new mapboxgl.Popup()
+    .setLngLat(coordinates)
+    .setHTML(description)
+    .addTo(map);
+});
+ 
+// Change the cursor to a pointer when the mouse is over the places layer.
+map.on('mouseenter', 'Solar (Status)', function () {
+    map.getCanvas().style.cursor = 'pointer';
+});
+ 
+// Change it back to a pointer when it leaves.
+map.on('mouseleave', 'Solar (Status)', function () {
     map.getCanvas().style.cursor = '';
 });
 
